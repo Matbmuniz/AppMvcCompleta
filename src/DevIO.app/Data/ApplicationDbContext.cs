138e3770b98dbab2643c2ff42c31b7a44a71264d
Matheus Muniz <matheussk2015@gmail.com>
2022-08-08 21:29:23 -0300

    Primeiro commit do Curso para guardar

---
+++ src/DevIO.app/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DevIO.app.ViewModels;
+
+namespace DevIO.app.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DevIO.app.ViewModels.FornecedorViewModel> FornecedorViewModel { get; set; }
+        public DbSet<DevIO.app.ViewModels.ProdutoViewModel> ProdutoViewModel { get; set; }
+        public DbSet<DevIO.app.ViewModels.EnderecoViewModel> EnderecoViewModel { get; set; }
+    }
+}
